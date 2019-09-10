b0904e544d6896a580ee85dcee029b0f5f26a4d7
sturtle333 <sturtle333@gmail.com>
2019-09-10 19:41:06 +0900

    19.09.10 prototype

--- docs/main.js
+++ docs/main.js
@@ -4,32 +4,38 @@ new Vue ({
     isInit: false,
     purpose: 0,
     lines: [],
-    timer: {
-      sec: 60
-    }
+    counting: null,
+    done: false
   },
   methods: {
     initial: function(){
       var main = this;
       main.lines = [];
-      main.isInit = true;
+      main.purpose = 0;
       for(var j=0;j<15;j++){
         var arr = [];
+        var hn = [];
         var line_index = j;
         for(var i=0;i<100;i++){
           var num = Math.random();
           num = num * 10;
           num = parseInt(num);
           arr.push(num);
+          if(i!=0){
+            hn.push((arr[i-1]+num)%10);
+          }
         }
         this.lines.push({
           array: arr,
           answer: [],
-          hint: [],
+          hint: hn,
           message: '',
           lineIndex: line_index,
           leftNum: 99,
           numberIndex: 0,
+          timer: 60,
+          timeSave: 60,
+          score: 0,
           next: function(){
             var i = main.purpose;
             main.purpose = i + 1;
@@ -37,6 +43,15 @@ new Vue ({
             var parseLeft = this.leftNum - 1;
             this.numberIndex = parseIndex;
             this.leftNum = parseLeft;
+            this.timeSave = this.timer;
+            this.scoring();
+            if(this.lineIndex < 14){
+              main.lines[this.lineIndex+1].timer = 60 + this.timeSave;
+              main.lines[this.lineIndex+1].start();
+            }
+            else {
+              main.done = true;
+            }
             setTimeout(function(){
               document.getElementById("input").focus();
             },10);
@@ -68,15 +83,39 @@ new Vue ({
             this.leftNum = parseLeft;
             this.message = '';
             this.answer.pop();
+          },
+          start: function() {
+            main.isInit = true;
+            setTimeout(function(){
+              document.getElementById("input").focus();
+            },10);
+            main.counting = setInterval(function(){
+              var temp;
+              if(main.lines[main.purpose].timer <= 0){
+                main.lines[main.purpose].next();
+              }
+              else {
+                temp = main.lines[main.purpose].timer - 1;
+              }
+              this.timer = temp;
+            }.bind(this),1000);
+          },
+          end: function() {
+            if(counting != null){
+              clearInterval(main.counting);
+            }
+          },
+          scoring: function(){
+            for(var i=0;i<this.answer.length;i++){
+              var scoreTemp = this.score;
+              if(this.hint[i] == this.answer[i]){
+                this.score = scoreTemp + 1;
+              }
+            }
           }
         });
       }
-    },
-    start: function() {
-      alert("start");
-    },
-    end: function() {
-      alert("end");
+      clearInterval(main.counting);
     }
   }
 })
