541ff1dadd9a7b855d1409aff3a40c9cf0d9e0bb
sturtle333 <sturtle333@gmail.com>
2019-09-24 21:12:43 +0900

    todoList

---
+++ src/components/Example.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-heading">Panel Heading without title</div>
+    <div class="panel-body">Panel content</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Example',
+  data(){
+    return {
+      msg: 'Basis!!!!'
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld.vue'
+import Example from '@/components/Example.vue'
+import TodoPage from '@/components/TodoPage.vue'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/example',
+      name: 'Example',
+      component: Example
+    },
+    {
+      path: '/todo',
+      name: 'TodoPage',
+      component: TodoPage
     }
   ]
 })
