b9dab38e3adfeb37f1d03b5cecc10728156dadf5
sturtle333 <sturtle333@gmail.com>
2019-09-25 12:43:37 +0900

    header

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="panel panel-default">
+    <nav-header></nav-header>
     <div class="panel-heading">Panel Heading without title</div>
     <div class="panel-body">Panel content</div>
   </div>
 </template>
 
 <script>
+import header from './child/header.js';
 export default {
   name: 'Example',
   data(){
     return {
       msg: 'Basis!!!!'
     }
+  },
+  component:{
+    'nav-header': header
   }
 }
 </script>
