384f9689c0776f5f555edb83243440bada50245d
sturtle333 <sturtle333@gmail.com>
2019-09-27 15:10:20 +0900

    190927/welcome-footer

---
+++ src/components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="footer">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'footer'
+  }
+</script>
+
+<style>
+#footer {
+  display:block;
+  background-color:
+}
+</style>
--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="welcome">
-    <div class="jumbotron text-center">
+    <div class="jumbotron text-center margin-space-top">
       <h1>Welcome!</h1>
       <p>
         해당 사이트는 UK test를 무료로 시험 볼 수 있는 사이트입니다.<br>별도의 회원가입, 유료구매 없이 UK test를 공부하세요!
       </p>
       <button type="button" class="btn btn-lg btn-link">바로가기</button>
     </div>
-    <div class="page-header">
+    <div class="page-header margin-space-top">
       <h1>How to do UK test?</h1>
     </div>
     <div class="description">
@@ -26,7 +26,7 @@
       <p>
         당신은, 주어진 1분 안에 하나의 수열을 풀어야합니다.
       </p>
-      <hr>
+      <hr class="margin-space">
       <p>
         풀이 방법은 간단합니다.
       </p>
@@ -37,13 +37,36 @@
       </p>
       <blockquote>
         <h2>Example</h2>
-        <p>3 6 4 2 6 8 4 0 7 8</p>
+        <p>수열 : 3 6 4 2 6 8 4 0 7 8</p>
         <p>3 + 6 = 9</p>
         <p>6 + 4 = 10 -> 0</p>
         <p>4 + 2 = 6</p>
         <p>2 + 6 = 8</p>
         <p>6 + 8 = 14 -> 4</p>
       </blockquote>
+      <p>
+        1분이 지나면 자동적으로 다음 수열로 넘어갑니다.
+      </p>
+      <p>
+        총 10개의 수열을 전부 풀면, 5분동안 휴식합니다.
+      </p>
+      <p>
+        그리고, 다시 10분동안 수열 10개를 풉니다.
+      </p>
+      <p>
+        수열 하나의 숫자는 총 100개입니다.
+      </p>
+    </div>
+    <div class="page-header margin-space-top">
+      <h1>패치노트</h1>
+    </div>
+    <div class="description">
+      <ol>
+        <li class="pathnote-contents">2019.09.10 프로토타입 제작</li>
+        <li class="pathnote-contents">2019.09.16 모바일 브라우져 지원</li>
+        <li class="pathnote-contents">2019.09.24 vue/cli로 컴포넌트 적용 및 리팩토링</li>
+        <li class="pathnote-contents">2019.09.27 Bootstrap 적용</li>
+      </ol>
     </div>
   </div>
 </template>
@@ -55,7 +78,19 @@ export default {
 </script>
 
 <style>
-text-center {
+.text-center {
   text-align: center;
+}
+.margin-space-top {
+  margin-top: 20rem;
+}
+.margin-space {
+  margin: 5rem;
+}
+.description {
+  font-size: 2rem;
+}
+.pathnote-contents {
+  margin: 2rem;
 }
 </style>
